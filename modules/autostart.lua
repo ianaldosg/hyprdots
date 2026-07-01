73222f95606f9ddf28bec3bb87498d12d22b8c7d
ianaldosg <ianaldosg@gmail.com>
2026-07-01 15:53:23 -0300

    Walker

--- modules/autostart.lua
+++ modules/autostart.lua
@@ -13,6 +13,9 @@ hl.on("hyprland.start", function()
     -- ─── Wallpaper ───────────────────────────────────────────────────────────
     hl.exec_cmd("hyprpaper")
 
+    -- ─── Walker ───────────────────────────────────────────────────────────
+    hl.exec_cmd("walker --gapplication-service")
+
     -- ─── Cursor ──────────────────────────────────────────────────────────────
     -- hl.exec_cmd("hyprctl setcursor Catppuccin-Mocha-Dark-Cursors 24")
 
